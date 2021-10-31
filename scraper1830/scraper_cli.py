a83e09572958803b872a5ed41757b1cf1520eb6a
Siddharth Venkatesh <siddharth.n.venkatesh@gmail.com>
2021-10-30 21:36:56 -0700

    Ran black formatter

--- scraper1830/scraper_cli.py
+++ scraper1830/scraper_cli.py
@@ -10,16 +10,19 @@ import click
 from .scraper1830 import Scraper1830
 
 
-    
 @click.group()
 def cli_entry():
     pass
 
+
 @cli_entry.command()
-@click.option('--id', prompt='Enter Game ID', help='The id for the 1830 game on 18xx.games')
+@click.option(
+    "--id", prompt="Enter Game ID", help="The id for the 1830 game on 18xx.games"
+)
 def plot_history(id):
     scraper = Scraper1830(id)
     scraper.plot_player_history()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli_entry()
