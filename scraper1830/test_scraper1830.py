dda5ede2c7436bbd7dee39f7f7c70e2456d13ac5
Siddharth Venkatesh <siddharth.n.venkatesh@gmail.com>
2021-10-30 18:04:29 -0700

    Move source code, test code to source folder

---
+++ scraper1830/test_scraper1830.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 23:43:10 2021
+
+@author: siddharthvenkatesh
+"""
+from ScraperClass import Scraper1830
+
+scraper = Scraper1830('60001')
+
+scraper.plot_player_history()
+
+# scraper.plot_player_history()
